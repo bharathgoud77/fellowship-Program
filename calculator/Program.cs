6e5cbda1a79c63a6ddcfa596510790d164dcd6ba
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-10-27 09:43:41 -0700

    caluclator

---
+++ calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calulator objCalulator = new Calulator();
+            Console.WriteLine("total Values =" + objCalulator.isSum(30, 20));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ calculator/calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+
+{
+    class Calulator
+    {
+        int totalValue;
+            public int isSum(int num1 , int num2)
+        {
+            return totalValue = num1 + num2;
+        }
+
+    }
+}
