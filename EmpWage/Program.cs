ec92d0d52f42e63806ae61dc14257f7f24626ee1
Bharathgoud77 <bharathgoud7702@gmail.com>
2021-10-29 03:43:46 -0700

    executed

---
+++ EmpWage/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Is_Full_Time = 1;
+            int Emp_Rate_Per_Hour = 10;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Is_Full_Time)
+            {
+                empHrs = 8;
+
+            }
+            else
+            {
+                empHrs = 0;
+
+
+            }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("empWage:" + empWage);
+            Console.ReadLine();
+
+        }
+    }
+}
